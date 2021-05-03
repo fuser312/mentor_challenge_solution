120ea7f84d4f67a1ab18989a483489a0baceb8f9
fuser312 <sherltonGT40000>
2021-05-03 18:16:05 +0530

    first part of the app complete

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
-const Card = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+import Title from "./Title";
 
-export default Card
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 250px;
+  /* height: 250px; */
+  background-color: #c4b2a9;
+  border-radius: 4px;
+  padding: 1rem;
+  margin: 1rem;
+
+  &:hover {
+    opacity: 0.5;
+    cursor: pointer;
+  }
+`;
+
+const Description = styled.p`
+  color: white;
+  text-align: center;
+`;
+
+const Card = ({ title, description }) => {
+  return (
+    <Wrapper>
+      <Title>{title}</Title>
+      <Description>{description}</Description>
+    </Wrapper>
+  );
+};
+
+export default Card;
